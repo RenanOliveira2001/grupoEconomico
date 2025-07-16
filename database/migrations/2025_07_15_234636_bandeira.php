ff0b30609d2289ad73662890da349bb00aa5e5e7
Renan Oliveira <r.c.oliveira@msn.com>
2025-07-15 21:25:03 -0300

    Commit inicial

---
+++ database/migrations/2025_07_15_234636_bandeira.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bandeira', function(Blueprint $table){
+            $table->id();
+            $table->text('nome');
+            $table->foreignId('grupo_economico');
+            $table->date('dt_criacao');
+            $table->dateTime('ultima_atualizacao');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bandeira');
+    }
+};
